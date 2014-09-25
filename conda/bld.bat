9df681a85a8248e36122e6b729950fe444525a5b
James Booth <jabooth@gmail.com>
2014-09-25 17:17:05 +0100

    first attempt at appveyor

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,6 +1,6 @@
-
+robocopy %RECIPE_DIR%\.. . /E
 set CONDA_ASSIMP_DIR=%LIBRARY_PREFIX%
-"%PYTHON%" setup.py install
+"%PYTHON%" setup.py install --single-version-externally-managed --record=%TEMP%record.txt
 if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
